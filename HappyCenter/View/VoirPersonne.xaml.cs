3b5fa91b10a15dadb32253992bc12fa210869ec2
Hounga François <Hounga Franois@DESKTOP-9M61J23>
2016-11-06 20:57:03 +0100

    Modification Planning d'activités(VoirUtilisateur)

--- HappyCenter/View/VoirPersonne.xaml.cs
+++ HappyCenter/View/VoirPersonne.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,12 +20,39 @@ namespace HappyCenter.View
     /// </summary>
     public partial class VoirPersonne : Window
     {
+
+        public class DataObject
+        {
+            public string A { get; set; }
+            public string B { get; set; }
+            public string C { get; set; }
+            public string D { get; set; }
+            public string E { get; set; }
+            public string F { get; set; }
+        }
         public VoirPersonne()
         {
            // HappyCenter.View.VoirPersonne vp = new View.VoirPersonne();
             InitializeComponent();
-           // vp.Show();
-           
+            // vp.Show();
+            var list = new ObservableCollection<DataObject>();
+            list.Add(new DataObject() { A = "", B = "MARDI", C = "MERCREDI", D = "JEUDI", E = "VENDREDI", F = "SAMEDI" });
+            list.Add(new DataObject() { A = "8h - 10 h", B = " ", C = " ", D = " ", E = " ", F = " " });
+            list.Add(new DataObject() { A = "10h - 12 h", B = " ", C = " ", D = " ", E = " ", F = " " });
+            list.Add(new DataObject() { A = "14h - 16 h", B = " ", C = " ", D = " ", E = " ", F = " " });
+            list.Add(new DataObject() { A = "16h - 18 h", B = " ", C = " ", D = " ", E = " ", F = " " });
+            this.planning.ItemsSource = list;
+
+
+        }
+
+        private void listView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+        private void planning_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
         }
 
         private void button2_Click(object sender, RoutedEventArgs e)
